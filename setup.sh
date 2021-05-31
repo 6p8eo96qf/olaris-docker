fb0ff84f6b8531885063fa623f12c4dee7632c0b
6p8eo96qf <85071673+6p8eo96qf@users.noreply.github.com>
2021-05-31 10:07:07 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wget -q https://downloads.rclone.org/v1.52.3/rclone-v1.52.3-linux-amd64.zip
+unzip -q rclone-v1.52.3-linux-amd64.zip
+export PATH=$PWD/rclone-v1.52.3-linux-amd64:$PATH
+if [[ -n $RCLONE_CONFIG]]; then
+	echo "Rclone config detected"
+	echo -e "$RCLONE_CONFIG" > ~/.config/rclone/rclone.conf
+fi
+echo "Updating olaris-server from nightlies"
+cd /opt
+curl -vL 'https://gitlab.com/api/v4/projects/olaris%2Folaris-server/jobs/artifacts/develop/download?job=dist-linux-amd64' > /opt/build.zip && unzip -o /opt/build.zip && unzip "olaris-linux-amd64-*.zip"
