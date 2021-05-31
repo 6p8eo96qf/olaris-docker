754025b1379b2007ef7fa2e03fe809b65fd7e929
6p8eo96qf <85071673+6p8eo96qf@users.noreply.github.com>
2021-05-31 13:35:47 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!bin/bash/
-export PORT=8080
 wget https://storage.googleapis.com/bysh-chef-files/olaris-release/olaris-linux-amd64-v0.3.3.zip
 unzip olaris-linux-amd64-v0.3.3.zip
 chmod +x bin/olaris
